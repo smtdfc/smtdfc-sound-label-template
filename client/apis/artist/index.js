7012ea133cf8be412f29d1a6f414780cf29da7d3
smtdfc <smtdfc.master@gmail.com>
2025-01-26 23:52:23 +0700

    update

--- client/apis/artist/index.js
+++ client/apis/artist/index.js
@@ -4,13 +4,24 @@ export class APIClientArtistManage {
     this.configs = configs;
     this.axiosInstance = app.axiosInstance;
     app.artist = this;
-    
+
   }
-  
-  async list(){
-    this.axiosInstance.post("/api/v1/artist/list",{},{
+
+  async list() {
+    this.axiosInstance.post("/api/v1/artist/list", {}, {
       withCredentials: true,
     })
+  }
+
+  async create(name = "", streams = {}) {
+    let { data } = this.axiosInstance.post("/api/v1/artist/create", {
+      name,
+      streams
+    }, {
+      withCredentials: true,
+    })
+
+    return data.results.info
   }
 
   static init(app, configs) {
--- client/pages/artist/index.js
+++ client/pages/artist/index.js
@@ -5,14 +5,13 @@ export const Page = Turtle.createComponent({
       this.app.router.redirect("/login", true);
       return;
     }
-
     this.userInfo = this.app.auth.user._info;
   },
 
-onRender(){
-this.app.api.client.artist.list()
-},
-  
+  onRender() {
+    this.app.api.client.artist.list()
+  },
+
   template() {
     return this.html`
       <div class="root fade-in " style="min-height:80vh;" ref="container">
@@ -21,8 +20,13 @@ this.app.api.client.artist.list()
           <div class="ml-3 d-flex flex-col">
             <h3 class="m-0">Artist Management</h3>
           </div>
+          <button class="ml-auto btn-icon  material-symbols-outlined">add</button>
+
         </div>
-        <div class="mt-5 mx-4 d-flex flex-col align-items-center">
+        <div class="mt-5 ">
+          <div class="container">
+            <input class="form-input" style="padding:0.6rem; width:100%; " placeholder="Search artist..."/>
+          </div>
         </div>
       </div>
     `;
