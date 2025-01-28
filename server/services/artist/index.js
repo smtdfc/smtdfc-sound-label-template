3c2e452590dcb73dd46d510f57499e7f73d3d480
smtdfc <smtdfc.master@gmail.com>
2025-01-28 15:04:23 +0700

    update index.js

--- server/services/artist/index.js
+++ server/services/artist/index.js
@@ -1,5 +1,21 @@
 const { generateID } = require("../../utils/id.js");
 
+const cloudinary = require("../../external/thirdparty/cloudinary")
+const fs = require('fs');
+const path = require('path');
+
+
+function solveAvatarPathFile(fileName) {
+  const filePath = path.join(__dirname, '../../../.data/uploads/avatar', fileName);
+
+  const uploadsDir = path.join(__dirname, '../../../.data/uploads/avatar');
+  if (!fs.existsSync(uploadsDir)) {
+    fs.mkdirSync(uploadsDir, { recursive: true });
+  }
+
+  return filePath
+}
+
 
 module.exports.list =  async function(userID){
   const list = await globalThis.Models.Artists.findAll({
@@ -14,11 +30,14 @@ module.exports.list =  async function(userID){
 }
 
 module.exports.create =  async function(name, legalName,streams={},avatar,createBy=""){
+  let fullPath = solveAvatarPathFile(avatar)
+  let avatarInfo = await cloudinary.uploadImage(fullPath)
+
   const info = await globalThis.Models.Artists.create({
     artistID:generateID(),
     name,
     legalName,
-    avatar,
+    avatar:avatarInfo.url,
     streams:JSON.stringify(streams),
     createBy
   });
